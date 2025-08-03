befc65c72d267848961181d5eac8810d7dbf9c14
Ваше Имя <_email@example.com>
2025-08-03 14:43:22 +0300

    init project

---
+++ src/user.entity.ts
@@ -0,0 +1,22 @@
+﻿import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  login: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column('int')
+  age: number;
+
+  @Column({ length: 1000 })
+  description: string;
+}
